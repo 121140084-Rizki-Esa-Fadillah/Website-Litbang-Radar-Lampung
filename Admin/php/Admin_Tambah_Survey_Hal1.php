2beec5b111b3f41812565875dbf5bca6924f9f17
RizkiEsaFadillah <rizkiifadillah456@gmail.com>
2024-08-07 00:34:34 +0800

    update

--- Admin/php/Admin_Tambah_Survey_Hal1.php
+++ Admin/php/Admin_Tambah_Survey_Hal1.php
@@ -4,13 +4,14 @@ include "Koneksi_survei_litbang.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $judul = $_POST['judul-survey'];
     $keterangan = $_POST['keterangan'];
+    $id_wilayah = $_POST['id_wilayah'];
     $image = $_FILES['upload-gambar']['name']; 
     $tmp = $_FILES['upload-gambar']['tmp_name']; 
 
     // Move uploaded file to a desired location
     $uploadDir = '../../image/';
     if (move_uploaded_file($tmp, $uploadDir . $image)) {
-        $query = "INSERT INTO survey (title, keterangan, image) VALUES ('$judul', '$keterangan', '$image')";
+        $query = "INSERT INTO survey (title, keterangan, id_wilayah, image) VALUES ('$judul', '$keterangan', '$id_wilayah', '$image')";
 
         if (mysqli_query($conn, $query)) {
             // Redirect to Admin_Main.html with query parameters
