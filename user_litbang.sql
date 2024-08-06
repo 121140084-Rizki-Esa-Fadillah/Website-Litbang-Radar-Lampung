bfab7081f305485176923fa0ed0f77c61bd5edc5
dimassaputra059 <dimas.121140059@student.itera.ac.id>
2024-08-06 11:09:18 +0700

    update

---
+++ user_litbang.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `user` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `username` varchar(25) NOT NULL,
+    `nama_lengkap` varchar(50) NOT NULL,
+    `email` varchar(75) NOT NULL,
+    `jenis_kelamin` varchar(20) NOT NULL,
+    `no_hp` varchar(20) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `role` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
