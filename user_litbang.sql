d03faeee289cd1dd96997abb977b114469669565
RizkiEsaFadillah <rizkiifadillah456@gmail.com>
2024-08-26 19:48:37 +0700

    Update

--- user_litbang.sql
+++ user_litbang.sql
@@ -1,14 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 26, 2024 at 02:47 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_litbang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
 CREATE TABLE `user` (
-    `id_user` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `username` varchar(25) NOT NULL,
-    `nama_lengkap` varchar(50) NOT NULL,
-    `email` varchar(75) NOT NULL,
-    `jenis_kelamin` varchar(20) NOT NULL,
-    `no_hp` varchar(20) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `role` varchar(5) NOT NULL,
-    `registered` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `last_login` TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-    `image_profile_name` varchar(255) NOT NULL,
-    `image_profile_path` varchar(255) NOT NULL
+  `id_user` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(5) NOT NULL,
+  `registered` datetime NOT NULL DEFAULT current_timestamp(),
+  `last_login` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `image_profile_name` varchar(255) NOT NULL,
+  `image_profile_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `nama_lengkap`, `email`, `jenis_kelamin`, `no_hp`, `password`, `role`, `registered`, `last_login`, `image_profile_name`, `image_profile_path`) VALUES
+(1, 'Admin', 'Admin Website Litbang', 'radarlitbang_123@gmail.com', 'Laki-Laki', '081234567890', 'Admin123', 'Admin', '2024-08-17 23:55:06', '2024-08-25 15:29:58', 'radar-logo.jpg', '../../image/foto-profile/5eebc546f3260d3f802792d925436f0e.jpg'),
+(5, 'User', 'User Website Litbang', 'userlitbang@yahoo.com', 'Laki-Laki', '081122334455', 'User_123', 'User', '2024-08-25 22:30:37', '2024-08-25 15:31:40', 'radar-logo.jpg', '../../image/foto-profile/49da432c4fa61cdfce9d1936a1971bb0.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
